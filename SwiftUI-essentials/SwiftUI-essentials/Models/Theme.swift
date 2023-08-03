042ea45db08f8c28c18944bb3a6400b32c5110c5
yeonsu <drawingbonny@gmail.com>
2023-08-03 09:18:10 +0900

    Creating a navigation hierarchy

--- SwiftUI-essentials/SwiftUI-essentials/Models/Theme.swift
+++ SwiftUI-essentials/SwiftUI-essentials/Models/Theme.swift
@@ -36,7 +36,12 @@ enum Theme: String {
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
+    
     var mainColor: Color {
         Color(rawValue)
+    }
+    
+    var name: String {
+        rawValue.capitalized
     }
 }
--- SwiftUI-essentials/SwiftUI-essentials/ScrumsView.swift
+++ SwiftUI-essentials/SwiftUI-essentials/ScrumsView.swift
@@ -11,12 +11,25 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
     
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                        .listRowBackground(scrum.theme.mainColor)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }
+
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
